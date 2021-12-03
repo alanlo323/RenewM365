d9dfc393ad8d876ac71da63e1c5aca324c042abd
Alan Lo <alanlo@gammonconstruction.com>
2021-12-03 16:05:41 +0800

    Change work flow

--- Program.cs
+++ Program.cs
@@ -14,40 +14,84 @@ namespace RenewM365
         {
             Task.Run(async () =>
             {
-                User user = await getUser();
-                DumpOptions dumpOptions = new DumpOptions
+                bool success = false;
+                do
                 {
-                    IgnoreDefaultValues = true
-                };
-                var userDump = ObjectDumper.Dump(user, dumpOptions);
-                Console.WriteLine($@"{userDump}");
-                Console.ReadKey();
+                    User user = await getUser();
+                    if (user != null)
+                    {
+                        success = true;
+
+                        DumpOptions dumpOptions = new DumpOptions
+                        {
+                            IgnoreDefaultValues = true
+                        };
+                        var userDump = ObjectDumper.Dump(user, dumpOptions);
+
+                        Console.Clear();
+
+                        Console.WriteLine();
+                        Console.WriteLine($@"{userDump}");
+                        Console.WriteLine($@"Call Graph API successfully, press any key to close this windows");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($@"Cannot get user info, press any key to retry");
+                        Console.ReadKey();
+
+                        Console.Clear();
+                    }
+                } while (!success);
             }).Wait();
 
             async static Task<User> getUser()
             {
-                // https://docs.microsoft.com/en-us/windows/communitytoolkit/services/graphlogin
-                string clientId = "your-clientId";
-                string tenantID = "your-tenatId";
-                string[] scopes = new string[] { "user.read" };
+                try
+                {
+                    // https://docs.microsoft.com/en-us/windows/communitytoolkit/services/graphlogin
 
-                IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder
-                            .Create(clientId)
-                            .WithTenantId(tenantID)
-                            .Build();
-                UsernamePasswordProvider authProvider = new UsernamePasswordProvider(publicClientApplication, scopes);
+                    //  Get Application (client) ID from Azure Portal - App registrations :
+                    //  https://portal.azure.com/#blade/Microsoft_AAD_RegisteredApps/ApplicationsListBlade
+                    Console.Write($@"Enter Application (client) ID:");
+                    string clientId = Console.ReadLine();
 
-                GraphServiceClient graphClient = new GraphServiceClient(authProvider);
-                Console.Write("Email:");
-                string email = Console.ReadLine();
-                Console.Write("Password:");
-                string password = Console.ReadLine();
-                Console.Write("Getting user info...");
-                SecureString securedPassword = new NetworkCredential(email, password).SecurePassword;
-                User me = await graphClient.Me.Request()
-                                .WithUsernamePassword(email, securedPassword)
-                                .GetAsync();
-                return me;
+                    //  Get TenantId inside application - Directory (tenant) ID
+                    Console.Write($@"Enter Directory (tenant) ID:");
+                    string tenantId = Console.ReadLine();
+
+                    string[] scopes = new string[] { "user.read" };
+
+                    IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder
+                                .Create(clientId)
+                                .WithTenantId(tenantId)
+                                .Build();
+                    UsernamePasswordProvider authProvider = new UsernamePasswordProvider(publicClientApplication, scopes);
+                    GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+                    Console.Write("Enter Email:");
+                    string email = Console.ReadLine();
+
+                    Console.Write("Enter Password:");
+                    string password = Console.ReadLine();
+
+                    Console.Clear();
+
+                    Console.WriteLine("Getting user info...");
+
+                    SecureString securedPassword = new NetworkCredential(email, password).SecurePassword;
+                    User me = await graphClient.Me.Request()
+                                    .WithUsernamePassword(email, securedPassword)
+                                    .GetAsync();
+                    return me;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+                return null;
             }
         }
     }
